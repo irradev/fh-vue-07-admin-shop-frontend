ce3ef305c00b072ab8a29c205309650151418ee6
irradev <israeldevcorel@gmail.com>
2024-11-17 23:32:01 -0600

    11:31AM

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+
+  <!-- Header Navbar -->
+  <top-menu />
+
+  <router-view />
+
+  <!-- Footer -->
+  <custom-footer />
+</template>
+
+<script setup lang="ts">
+  import TopMenu from '../components/TopMenu.vue';
+  import CustomFooter from '../components/CustomFooter.vue';
+</script>
